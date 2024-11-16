48ee6fbeb2f7031025928268d6b6f65e248f01a4
박성빈 <psb106305@gmail.com>
2024-11-17 02:58:53 +0900

    refactor: Observer Pattern 추상 클래스로 수정

--- koala/utils/observer.py
+++ koala/utils/observer.py
@@ -4,7 +4,9 @@ https://refactoring.guru/ko/design-patterns/observer
 """
 
 from __future__ import annotations
+
 from abc import ABC, abstractmethod
+from typing import List
 
 
 class Observer(ABC):
@@ -25,23 +27,30 @@ class Subject(ABC):
     The Subject interface declares a set of methods for managing subscribers.
     """
 
-    @abstractmethod
+    def __init__(self):
+        self._observers: List[Observer] = []
+
     def attach(self, observer: Observer) -> None:
         """
         Attach an observer to the subject.
         """
-        pass
+        self._observers.append(observer)
 
-    @abstractmethod
     def detach(self, observer: Observer) -> None:
         """
         Detach an observer from the subject.
         """
-        pass
+        self._observers.remove(observer)
 
-    @abstractmethod
     def notify(self) -> None:
         """
         Notify all observers about an event.
         """
-        pass
+        for observer in self._observers:
+            observer.update(self)
+
+
+__all__ = (
+    'Observer',
+    'Subject',
+)
