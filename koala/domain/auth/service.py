97059ebe57938b5d25c33af0ca9be1ab537cd1eb
박성빈 <psb106305@gmail.com>
2024-11-17 02:42:13 +0900

    feat: LoginService 뼈대 작성

---
+++ koala/domain/auth/service.py
@@ -0,0 +1,26 @@
+from http.cookies import SimpleCookie
+
+
+class LoginService:
+
+    def __init__(self,
+                 portal_id: str,
+                 portal_pw: str,
+                 portal_ip: str,
+                 cookie: SimpleCookie
+                 ):
+        self.portal_id = portal_id
+        self.portal_pw = portal_pw
+        self.portal_ip = portal_ip
+        self.cookie = cookie
+
+    async def login(self):
+        ...
+
+    async def refresh(self):
+        ...
+
+
+__all__ = (
+    'LoginService',
+)
