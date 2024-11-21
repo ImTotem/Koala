fdb9e16184d2c37b805e0259d7c314c4738c235f
박성빈 <psb106305@gmail.com>
2024-11-21 14:38:47 +0900

    feat: 스케줄러 의존성 주입

--- koala/di/auth.py
+++ koala/di/auth.py
@@ -1,21 +1,21 @@
-from http.cookies import SimpleCookie
-
 from dependency_injector import containers, providers
 
+from koala.domain.auth.model import Cookies
 from koala.domain.auth.service import LoginService
 
 
 class _AuthContainer(containers.DeclarativeContainer):
     config = providers.Configuration()
 
-    cookie = providers.ThreadSafeSingleton(SimpleCookie)
+    cookies = providers.ThreadSafeSingleton(Cookies)
 
     service = providers.ThreadSafeSingleton(
         LoginService,
-        portal_id=config.id,
-        portal_pw=config.password,
-        portal_ip=config.ip,
-        cookie=cookie,
+        portal_id=config.portal.id,
+        portal_pw=config.portal.password,
+        portal_ip=config.portal.ip,
+        cookies=cookies,
+        timezone=config.timezone,
     )
 
 
--- koala/di/di.py
+++ koala/di/di.py
@@ -2,6 +2,7 @@ from dependency_injector import containers, providers
 
 from koala.conf.config import ConfigContainer
 from koala.di.auth import _AuthContainer
+from koala.domain.scheduler.manager import ScheduleManager
 
 
 class DI(containers.DeclarativeContainer):
@@ -9,7 +10,12 @@ class DI(containers.DeclarativeContainer):
 
     auth = providers.Container(
         _AuthContainer,
-        config=config.portal,
+        config=config,
+    )
+
+    schedule_manager = providers.ThreadSafeSingleton(
+        ScheduleManager,
+        login_service=auth.service,
     )
 
 
