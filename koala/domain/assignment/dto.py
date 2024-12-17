adb8b90a035a25b7d494a6a79c8857957e9262ac
byeok99 <tkdqur3203@gmail.com>
2024-12-17 20:51:02 +0900

    feat: DTO 기능 구현

--- koala/domain/assignment/dto.py
+++ koala/domain/assignment/dto.py
@@ -1,4 +1,7 @@
 import datetime
+from collections import defaultdict
+from dataclasses import field
+from typing import Union, DefaultDict
 
 from koala.domain.assignment.model import BaseAssignment
 from datetime import datetime, timedelta  # 날짜 및 시간 관련 기능을 위한 datetime 모듈 임포트
@@ -6,21 +9,26 @@ from datetime import datetime, timedelta  # 날짜 및 시간 관련 기능을 �
 
 # TODO : DTO 작성하기
 class AssignmentDTO:
-
-    def __init__(self, id: int, url: str, title: str, end_date: datetime):
+    def __init__(self, name: str, id: int, url: str, title: str, start_date: datetime, end_date: datetime, progress: int):
+        self.name = name
         self.id = id
         self.url = url
         self.title = title
+        self.start_date = start_date
         self.end_date = end_date
+        self.progress = progress
 
     @classmethod
-    def factory(cls, assignment: BaseAssignment):
+    def factory(cls, name, assignment: BaseAssignment):
         # TODO : 시간 A보정
         return cls(
+            name=name,
             id=assignment.id,
             url=assignment.url,
             title=assignment.title,
-            end_date=cls.adjust_time(assignment.end_date)
+            start_date=assignment.start_date,
+            end_date=cls.adjust_time(assignment.end_date),
+            progress=assignment.progress
         )
 
     @staticmethod
